94fb48f1f3fd524d040f1d08f7901c33296c9519
Jean Carlos Esperança <30536779+jean-esperanca@users.noreply.github.com>
2023-06-16 00:35:22 -0300

    Pages, router commit

---
+++ pages/products/[id].vue
@@ -0,0 +1,8 @@
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<template>
+    <p>Products details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sapiente iusto, ipsam ducimus debitis temporibus! Aliquam ducimus numquam placeat eum, dolore optio vero ab reprehenderit consectetur tenetur dolores dolorem sapiente!</p>
+</template>
---
+++ pages/products/index.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+    <h2>Products</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat quam aperiam odio nesciunt repellat a dolores. Eos, accusantium dignissimos, aut nulla ex a laboriosam, tempore obcaecati nam ab voluptatum earum.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat quam aperiam odio nesciunt repellat a dolores. Eos, accusantium dignissimos, aut nulla ex a laboriosam, tempore obcaecati nam ab voluptatum earum.</p>
+</template>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+</style>
